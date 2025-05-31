5040fe3be1b39218db217e90db46b093cc0b1c1c
realCheesyQuesadilla <debian.dave@pm.me>
2025-05-30 22:14:52 -1000

    correctly detects events

--- shadowmonitor.c
+++ shadowmonitor.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/inotify.h>
+#include <unistd.h>
 
 #define WATCHED_FILE "/etc/shadow"
-#define EVENT_SIZE (sizeof(struct inotify_event))
+#define BUFFER_SIZE (sizeof(struct inotify_event))
 
 
 int main() {
@@ -12,6 +13,9 @@ int main() {
     // check watch return
     int watch_fd = 0;
 
+    // make buffer for inotify_event
+    char buffer[BUFFER_SIZE];
+    
     printf("Shadow monitor initialized\n");
     printf("Monitoring: %s\n", WATCHED_FILE);
     
@@ -21,11 +25,50 @@ int main() {
     if (inotify_fd < 0)
     {
         printf("Error opening inotify\n");
-        close(inotify_fd);
         exit(EXIT_FAILURE);
     }
 
-    printf("inotify successful\n");
+    // now to make watch
+    watch_fd = inotify_add_watch(inotify_fd, WATCHED_FILE, IN_MODIFY);
 
+    if (watch_fd < 0)
+    {
+        printf("Error reserving watcher\n");
+        printf("Is this running in root?\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully monitoring %s using watch descriptor %d \n", WATCHED_FILE, watch_fd);
+    printf("Running...\n Enter CTRL+C to stop\n");
+
+    while(1)
+    {
+        // read inotify fd
+        int length = read(inotify_fd, buffer, BUFFER_SIZE);
+        if (length < 0 )
+        {
+            printf("Error reading\n");
+            break;
+        }
+
+        // process any events
+        int i = 0;
+        while (i < length)
+        {
+            struct inotify_event* event = (struct inotify_event *) buffer+i;
+
+            printf("Event detected\n");
+            
+            // bitwise math to verify event was modify
+            if(event->mask & IN_MODIFY)
+            {
+                printf("shadow was modified\n");
+            }
+        }
+
+    }
+
+    inotify_rm_watch(inotify_fd, watch_fd);
+    close(inotify_fd);
     return 0;
 }
